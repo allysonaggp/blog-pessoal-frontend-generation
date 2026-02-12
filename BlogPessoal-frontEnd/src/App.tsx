08c8884211e660addcdff2bb62e02686179496d9
allysonaggp <allysonaggp@gmail.com>
2026-02-11 22:30:49 -0300

    adicionado pagina home.tsx e componentes navbar e footer

--- BlogPessoal-frontEnd/src/App.tsx
+++ BlogPessoal-frontEnd/src/App.tsx
@@ -1,6 +1,14 @@
 import Home from "./pages/home/home";
+import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 export default App;
---
+++ BlogPessoal-frontEnd/src/components/navbar/navbar.tsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <>
+      <div className="w-full flex justify-center py-4 bg-indigo-900 text-white">
+        <div className="container flex justify-between text-lg mx-8">
+          Blog Pessoal
+          <div className="flex gap-4">
+            <p>Postagens</p>
+            <p>Temas</p>
+            <p>Cadastrar</p>
+            <p>TemasPerfil</p>
+            <p>Sair</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Navbar;
