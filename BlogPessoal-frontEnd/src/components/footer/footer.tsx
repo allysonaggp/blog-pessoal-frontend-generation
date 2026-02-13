80fd5a89012599f1d0fac82d9ae7d0b891586885
allysonaggp <allysonaggp@gmail.com>
2026-02-13 00:10:32 -0300

    Adicionado componente Cadastro e Login

--- BlogPessoal-frontEnd/src/components/footer/footer.tsx
+++ BlogPessoal-frontEnd/src/components/footer/footer.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react";
+import { FacebookLogoIcon, GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from "@phosphor-icons/react";
 
 function footer() {
   const data = new Date().getFullYear();
@@ -9,9 +9,15 @@ function footer() {
           <p className="text-xl font-bold">Blog Pessoal Generation | Copyright: {data}</p>
           <p className="text-lg">Acesse nossas redes sociais</p>
           <div className="flex gap-2">
-            <LinkedinLogoIcon size={48} weight="bold" />
-            <InstagramLogoIcon size={48} weight="bold" />
-            <FacebookLogoIcon size={48} weight="bold" />
+            <a href="https://www.linkedin.com/in/allysongon%C3%A7alves/" target="_blank">
+              <LinkedinLogoIcon size={48} weight="bold" />
+            </a>
+            <a href="https://www.instagram.com/allysonggp/" target="_blank">
+              <InstagramLogoIcon size={48} weight="bold" />
+            </a>
+            <a href="https://github.com/allysonaggp" target="_blank">
+              <GithubLogoIcon size={45} weight="bold" />
+            </a>
           </div>
         </div>
       </div>
